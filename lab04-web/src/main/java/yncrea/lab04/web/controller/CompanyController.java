4fe170d85ba68feed619df7ffad1e468fce33cb4
Unknown <vincentmorel44444@gmail.com>
2020-01-29 01:14:05 +0100

    Deployement 2
    Second deployement is functional, we can submit a new company.

--- lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
+++ lab04-web/src/main/java/yncrea/lab04/web/controller/CompanyController.java
@@ -2,7 +2,10 @@ package yncrea.lab04.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import yncrea.lab04.core.dao.CompanyDAO;
 import yncrea.lab04.core.entity.Company;
 import yncrea.lab04.core.service.CompanyService;
 
@@ -17,11 +20,27 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
-    @RequestMapping("/list")
+    @RequestMapping(value="/list")
     public String getListOfCompanies(ModelMap modelMap) {
 
         List<Company> companiesList = companyService.findAllWithProjects();
         modelMap.put("companies", companiesList);
         return "companiesList";
     }
+
+    @RequestMapping(value="/form")
+    public String getForm(ModelMap modelMap) {
+        Company company = new Company();
+        modelMap.put("company", company);
+        return "companyForm";
+    }
+
+    @RequestMapping(value = "/form", method = RequestMethod.POST)
+    public String submitForm(@ModelAttribute("company") Company company) {
+
+        companyService.save(company);
+        return "redirect:list";
+    }
+
+
 }
